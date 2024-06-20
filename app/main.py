843ef077b3be434c4528e857e34d54ba558e3572
yeiserJimenez <yeiser192@gmail.com>
2024-06-20 01:47:26 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,67 +1,26 @@
 import sys
 import os
+from .utils import *
 
-def get_command_message(command):
-    return " ".join(command[1:])
 
-def is_builtin_command(command, builtins):
-    return command in builtins
+class Shell:
+    def __init__(self):
+        self.handler = CommandHandler(
+            busybox={"exit": "exit", "echo": "echo", "type": "type"},
+            scripts=["cat"],
+            path=os.environ.get("PATH", ""),
+        )
 
-def is_script_command(command, scripts):
-    return command in scripts
+    def run(self):
+        while True:
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
+            command_input = input().strip()
 
-def find_command_in_path(command, paths):
-    for path in paths:
-        cmd_path = os.path.join(path, command)
-        if os.path.isfile(cmd_path):
-            return cmd_path
-    return None
+            if not self.handler.handle_command(command_input):
+                break
 
-def main():
-    busybox = {
-        "exit": "exit",
-        "echo": "echo",
-        "type": "type"
-    }
-
-    scripts = ["cat"]
-    PATH = os.environ.get("PATH", "")
-    paths = PATH.split(":")
-
-    while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
-        command_input = input().strip()
-
-        if command_input in ("exit", "exit 0"):
-            break
-
-        command_parts = command_input.split()
-        if not command_parts:
-            continue
-
-        command = command_parts[0]
-
-        if command == busybox["echo"]:
-            sys.stdout.write(f"{get_command_message(command_parts)}\n")
-            if os.path.isfile(command.split(" ")[0]):
-                    os.system(command)
-            else:
-                print(f"{command}: command not found")
-        elif command == busybox["type"]:
-            command_to_find = command_parts[1] if len(command_parts) > 1 else ""
-            if is_builtin_command(command_to_find, busybox):
-                sys.stdout.write(f"{command_to_find} is a shell builtin\n")
-            elif is_script_command(command_to_find, scripts):
-                sys.stdout.write(f"{command_to_find} is /bin/{command_to_find}\n")
-            else:
-                cmd_path = find_command_in_path(command_to_find, paths)
-                if cmd_path:
-                    sys.stdout.write(f"{command_to_find} is {cmd_path}\n")
-                else:
-                    sys.stdout.write(f"{command_to_find}: not found\n")
-        else:
-            sys.stdout.write(f"{command}: command not found\n")
 
 if __name__ == "__main__":
-    main()
+    shell = Shell()
+    shell.run()
